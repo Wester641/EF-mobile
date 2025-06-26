b98c588f2ac10a17ff43004863a892ba8203130a
Asadbek Abdufattaev <asad@Asadbeks-Mac-mini.local>
2025-06-26 12:33:33 +0600

    EMT-2 | Refactor

--- test/specs/dashboard/EMT-2/Selectors.ts
+++ test/specs/dashboard/EMT-2/Selectors.ts
@@ -7,10 +7,24 @@ export class Selectors {
   static readonly startInspection = byValueKey("start_inspection");
   static readonly addIssue = byValueKey("add_issue");
 
+  static readonly profileButton = byValueKey("profile_button");
+  
+  static readonly unitsViewAll = byValueKey("units_view_all");
+  static readonly issueViewAll = byValueKey("issue_view_all");
+
+  static unitItem(index: number) {
+    return byValueKey(`unit_${index}`);
+  }
+  
+  static issueItem(index: number) {
+    return byValueKey(`issue_${index}`);
+  }
+
   // Bottom Bar: Bottom navigation
   static readonly homeTab = byValueKey("home_tab");
   static readonly browseTab = byValueKey("browse_tab");
 
     // App Bar: App Navigation
   static readonly backButton = byValueKey("back_button");
+  static readonly appbarTitle = byValueKey("appbar_title");
 }
